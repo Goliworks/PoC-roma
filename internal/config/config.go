05ba894c59f0d79484a14e13f0e4c535154fa026
Guillaume (Goliver) Golay <goliworks@gmail.com>
2020-05-15 02:09:55 +0200

    feat: simple reverse proxy

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,10 +4,21 @@ const (
 	defaultHttpPort = 80
 )
 
+type Destinations map[string]string
+
 type Config struct {
 	Port uint16
+	Destinations
 }
 
 func NewConfig() *Config {
-	return &Config{defaultHttpPort}
+	cfg := new(Config)
+	cfg.Port = defaultHttpPort
+	cfg.Destinations = make(Destinations)
+
+	// temporary test
+	cfg.Destinations["dev1.test"] = "localhost:3500"
+	cfg.Destinations["dev2.test"] = "localhost:3000"
+
+	return cfg
 }
--- internal/server/handler.go
+++ internal/server/handler.go
@@ -2,9 +2,22 @@ package server
 
 import (
 	"fmt"
+	"github.com/Goliworks/Roma/internal/config"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
 
 func Handler (w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, Roma!")
+	cfg := config.NewConfig()
+
+	ptc := "http"
+	inUrl, _ := url.Parse(fmt.Sprintf("%v://%v", ptc, r.Host))
+	destUrl, _ := url.Parse(fmt.Sprintf("http://%v", cfg.Destinations[inUrl.Hostname()]))
+
+	fmt.Printf("incoming host : %v\n", r.Host)
+	fmt.Printf("destination host : %v\n", destUrl.Host)
+
+	proxy := httputil.NewSingleHostReverseProxy(destUrl)
+	proxy.ServeHTTP(w, r)
 }
